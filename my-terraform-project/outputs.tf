545363fdde57cc9ad40626d9b0b49635945abfb9
takwa2001 <sahli.takwa@enis.tn>
2024-12-10 12:56:23 +0100

    Initial commit

--- my-terraform-project/outputs.tf
+++ my-terraform-project/outputs.tf
@@ -34,3 +34,16 @@ output "instance_details" {
     subnet_id            = aws_instance.public_instance.subnet_id
   }
 }
+output "deployer_key_s3_uri" {
+  description = "S3 URI of the deployer key file"
+  value       = "s3://${aws_s3_object.private_key_object.bucket}/${aws_s3_object.private_key_object.key}"
+}
+
+output "rds_endpoint" {
+  description = "The endpoint of the RDS database"
+  value = {
+    endpoint       = aws_db_instance.mydb.endpoint
+    username       = aws_db_instance.mydb.username
+    db_instance_id = aws_db_instance.mydb.id
+  }
+}
